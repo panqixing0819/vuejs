9cd94dd72da498b7878490f34c2457b49c2abfec
javascript12345 <1071438646@qq.com>
2017-05-16 23:08:25 +0800

    delete

--- static/js/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementsByTagName('html')[0].style.fontSize=window.screen.width/32+'px';
-window.onresize=function(){
-	document.getElementsByTagName('html')[0].style.fontSize=window.screen.width/32+'px';
-}
