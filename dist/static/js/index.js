6e40938e316b69444c48eb666518e7ae5d8c1dd8
javascript12345 <1071438646@qq.com>
2017-05-16 22:24:31 +0800

    项目代码

---
+++ dist/static/js/index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName('html')[0].style.fontSize=window.screen.width/32+'px';
+window.onresize=function(){
+	document.getElementsByTagName('html')[0].style.fontSize=window.screen.width/32+'px';
+}
